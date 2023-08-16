4c4f0692c9a8cd298ec90e5675a9085997e910b3
Nadav Ben Haim <nadavbh@microsoft.com>
2023-08-16 16:21:54 +0300

    initial commit

---
+++ test.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title>Test Website</title>
+ </head>
+ <body>
+<body bgcolor="#f1f1f1" text="Blue">
+<h1>I'm just a test webpage</h1>
+<?php
+echo "<br>";
+echo 'Thank you for attending my Learning Thursday about Containers! ';
+echo "<br>";
+
+?> 
+ </body>
+</html>
